98f53bc651d411396d7f45b871cdaaa73641ceee
Piotr Synowiec <psynowiec@gmail.com>
2016-10-12 18:47:24 +0200

    heroku

---
+++ src/mysiar/Bundle/InvoiceBundle/HerokuDatabase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Piotr Synowiec (c) 2016 psynowiec@gmail.com
+ *
+ * Date: 2016-10-12
+ * Time: 18:32
+ */
+
+
+
+namespace mysiar\Bundle\InvoiceBundle;
+
+use Composer\Script\Event;
+
+
+class HerokuDatabase
+{
+    public static function populateEnvironment(Event $event)
+    {
+        $url = getenv("DATABASE_URL");
+
+        if ($url) {
+            $url = parse_url($url);
+            putenv("DATABASE_HOST={$url['host']}");
+            putenv("DATABASE_USER={$url['user']}");
+            putenv("DATABASE_PASSWORD={$url['pass']}");
+            $db = substr($url['path'],1);
+            putenv("DATABASE_NAME={$db}");
+        }
+
+        $io = $event->getIO();
+
+        $io->write("DATABASE_URL=".getenv("DATABASE_URL"));
+    }
+}
