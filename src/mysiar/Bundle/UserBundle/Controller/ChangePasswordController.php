cd89ad162e6947cfbb0d0e9d2025d6fa168181db
Piotr Synowiec <psynowiec@gmail.com>
2016-10-12 16:47:32 +0200

    Password change formated

--- src/mysiar/Bundle/UserBundle/Controller/ChangePasswordController.php
+++ src/mysiar/Bundle/UserBundle/Controller/ChangePasswordController.php
@@ -2,55 +2,51 @@
 /**
  * Piotr Synowiec (c) 2016 psynowiec@gmail.com
  *
- * Date: 2016-10-11
- * Time: 19:15
- *
- *
- * http://symfony.com/doc/current/bundles/FOSUserBundle/overriding_controllers.html
+ * Date: 2016-10-12
+ * Time: 13:34
  *
  * overrides FOSUser Controller
  */
 
 namespace mysiar\Bundle\UserBundle\Controller;
 
+use FOS\UserBundle\Controller\ChangePasswordController as BaseController;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use FOS\UserBundle\Model\UserInterface;
-use FOS\UserBundle\Controller\ProfileController as BaseController;
 
 
-
-class ProfileController extends BaseController
+class ChangePasswordController extends BaseController
 {
-
     /**
-     * Edit the user
+     * Change user password
      */
-    public function editAction()
+    public function changePasswordAction()
     {
         $user = $this->container->get('security.context')->getToken()->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
-        $form = $this->container->get('fos_user.profile.form');
-        $formHandler = $this->container->get('fos_user.profile.form.handler');
+        $form = $this->container->get('fos_user.change_password.form');
+        $formHandler = $this->container->get('fos_user.change_password.form.handler');
 
         $process = $formHandler->process($user);
         if ($process) {
-            $this->setFlash('fos_user_success', 'profile.flash.updated');
+            $this->setFlash('fos_user_success', 'change_password.flash.success');
 
             return new RedirectResponse($this->getRedirectionUrl($user));
         }
 
         return $this->container->get('templating')->renderResponse(
-            'FOSUserBundle:Profile:edit.html.'.$this->container->getParameter('fos_user.template.engine'),
+            'FOSUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
             array(
                 'form' => $form->createView(),
                 'user' => $user // passing extra var to render top_menu
-            )
+                )
         );
     }
+
 
 }
